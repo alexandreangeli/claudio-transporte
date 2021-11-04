3e0af1c531092b88de198eabb96d515ae23de79f
Alexandre Angeli de Lima <alexandre.lima@hiplatform.com>
2021-11-04 18:47:55 -0300

    Removed .vue from icon imports

--- plugins/vue-material-icons.js
+++ plugins/vue-material-icons.js
@@ -1,9 +1,9 @@
 import Vue from "vue";
-import MapMarker from "vue-material-design-icons/mapMarker.vue";
-import Phone from "vue-material-design-icons/phone.vue";
-import Facebook from "vue-material-design-icons/facebook.vue";
-import Instagram from "vue-material-design-icons/instagram.vue";
-import Whatsapp from "vue-material-design-icons/whatsapp.vue";
+import MapMarker from "vue-material-design-icons/mapMarker";
+import Phone from "vue-material-design-icons/phone";
+import Facebook from "vue-material-design-icons/facebook";
+import Instagram from "vue-material-design-icons/instagram";
+import Whatsapp from "vue-material-design-icons/whatsapp";
 
 export default function materialIcons() {
   Vue.component("map-marker-icon", MapMarker);
