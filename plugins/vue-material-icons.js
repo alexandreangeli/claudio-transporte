0c261b9b68c1af943e62fdde6c795b877f953237
Alexandre Angeli de Lima <alexandre.lima@hiplatform.com>
2021-11-04 18:54:05 -0300

    Capitalized icon import names

--- plugins/vue-material-icons.js
+++ plugins/vue-material-icons.js
@@ -1,9 +1,9 @@
 import Vue from "vue";
-import MapMarker from "vue-material-design-icons/mapMarker";
-import Phone from "vue-material-design-icons/phone";
-import Facebook from "vue-material-design-icons/facebook";
-import Instagram from "vue-material-design-icons/instagram";
-import Whatsapp from "vue-material-design-icons/whatsapp";
+import MapMarker from "vue-material-design-icons/MapMarker";
+import Phone from "vue-material-design-icons/Phone";
+import Facebook from "vue-material-design-icons/Facebook";
+import Instagram from "vue-material-design-icons/Instagram";
+import Whatsapp from "vue-material-design-icons/Whatsapp";
 
 export default function materialIcons() {
   Vue.component("map-marker-icon", MapMarker);
